1191cc9c5bb71e2f829d5c1ada0e3ba9f8aea51f
Martha659 <martha.blackcloud@students.msswarriors.org>
2025-12-04 20:02:01 +0000

    11

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const school = [
+    'name': 'average class size', 
+    'mascot':
+]
+
+document.getElementById("result").innerHTML = "${name} has an average class size of ${average class size} and their mascot is a ${mascot}"
+
+
 
