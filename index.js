792522eef6c0411e6578ee2df2b111def3eecff5
Martha659 <martha.blackcloud@students.msswarriors.org>
2025-12-05 19:29:09 +0000

    1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const school = {
-    'name', 
+    'name',
     'average class size',
     'mascot'
 };
 
 
-document.getElementById("result").innerHTML = "${name} has an average class size of ${average.class.size} and their mascot is a ${mascot}";
+document.getElementById("result").innerHTML = "${name} has an average class size of ${average class size} and their mascot is a ${mascot}";
 
 
 
