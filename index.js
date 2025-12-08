da7b85939eaa4e8602c7189125656200379d7535
Martha659 <martha.blackcloud@students.msswarriors.org>
2025-12-08 19:31:04 +0000

    98

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-
-const school = {
-  name: "Riverdale High",
-  averageClassSize: 25,
-  mascot: "Bulldog"
+const school = { 
+    name: 'meskwaki'
+    averageClassSize: 3
+    mascot: 'hawk'
 };
 
-
-document.getElementById("result").innerHTML =
-  `${school.name} has an average class size of ${school.averageClassSize} and their mascot is a ${school.mascot}.`;
+document.getElementById("result").innerHTML = `${school.name} has an average class size of ${school.averageClassSize} and their mascot is a ${school.mascot}`;
